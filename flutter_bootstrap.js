d89a842db1f4755979ecd4c94874241e294e01cb
Srijen-Raja <srijenraja@gmail.com>
2025-07-26 17:02:31 +0530

    Firebase Integration

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "318333187"
+    serviceWorkerVersion: "2503905946"
   }
 });
